6dfd54d908e773ec2d85b6959ea27ab50a55f769
jlal1226 <jlal1226@naver.com>
2022-02-13 18:52:49 +0900

    로그인 버튼 구현 및 IndexController에서 userName을 model에 저장하는 코드 구현

--- src/main/java/com/springProject/springboot/web/IndexController.java
+++ src/main/java/com/springProject/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springProject.springboot.web;
 
+import com.springProject.springboot.config.auth.dto.SessionUser;
 import com.springProject.springboot.service.posts.PostsService;
 import com.springProject.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // "index"를 반환하면 index.mustache로 전환되어 View Resolver가 처리함
     @GetMapping("/")
     public String index(Model model) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다
         model.addAttribute("posts", postsService.findAllDesc()); // 결과를 posts로 index.mutache에 전달 -> posts라는 List
+
+        // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser를 저장
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        // 세션에 저장된 값이 있을 때만 model에 userName을 등록
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
